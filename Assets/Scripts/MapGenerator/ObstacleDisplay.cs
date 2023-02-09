f23dc9c2f364f8a6b6d10511a86494d8684674eb
Alex Kopareiko <sanyakopar@gmail.com>
2023-02-09 23:31:57 +0200

    obstacle quantity correction

--- Assets/Scripts/MapGenerator/ObstacleDisplay.cs
+++ Assets/Scripts/MapGenerator/ObstacleDisplay.cs
@@ -9,6 +9,8 @@ public class ObstacleDisplay : MonoBehaviour
     [SerializeField] float top;
     [SerializeField] float bottom;
     [SerializeField] float radiusToAvoid = 2f;
+    [SerializeField] int obstacleAllowed = 20;
+    [SerializeField] float roadWidth = 1f;
     [SerializeField] GameObject obstaclePref;
     [SerializeField] Transform playerTrans;
     [SerializeField] Transform finishTrans;
@@ -23,12 +25,20 @@ public class ObstacleDisplay : MonoBehaviour
 
         Vector3 position;
         float distance;
+        int obstacleCount = 0;
+
+        float leftRoadPoint = playerTrans.position.x - roadWidth;
+        float rightRoadPoint = playerTrans.position.x + roadWidth;
+        float topRoadPoint = finishTrans.position.z;
+        float bottomRoadPoint = playerTrans.position.z;
 
 
         float delimiterHor = (right - left) / (width);
         float delimiterVert = (top - bottom) / (height);
 
         obstacles = new GameObject("obstacles");
+
+
 
         for (int y = 0; y < height; y++)
         {
@@ -38,10 +48,25 @@ public class ObstacleDisplay : MonoBehaviour
                 if (noiseMap[x, y] < 0.5f) continue;
                 //Debug.Log("noiseMap[x, y] " + noiseMap[x, y]);
                 position = new Vector3(left + x * delimiterHor, 0, bottom + y * delimiterVert);
+
+                // not allowed to pit obstacles near player and finish
                 distance = Vector3.Distance(playerTrans.position, position);
                 if (distance < radiusToAvoid) continue;
                 distance = Vector3.Distance(finishTrans.position, position);
                 if (distance < radiusToAvoid) continue;
+
+                #region correction
+                // if obstacle is positioned inside road
+                if ( position.x > leftRoadPoint && position.x < rightRoadPoint && 
+                    position.z > bottomRoadPoint && position.z < topRoadPoint)
+                {
+                    obstacleCount++;
+                    //Debug.Log("obstacleCount " + obstacleCount);
+                    //return if too many obstacles
+                    if (obstacleCount > obstacleAllowed) continue;
+                }
+                #endregion
+
                 obstacle = Instantiate(obstaclePref, position, Quaternion.identity);
                 obstacle.transform.localScale = obstacle.transform.localScale * noiseMap[x, y] * 2;
                 obstacle.transform.SetParent(obstacles.transform);
